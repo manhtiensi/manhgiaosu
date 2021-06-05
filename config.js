28a4120a1535626ef039be49553e14279182f69a
manhgg22 <85389534+manhgg22@users.noreply.github.com>
2021-06-05 16:12:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Huyền à!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường,
     Mà sao em cứ đi nhầm đường,
     Lạc vào tim anh lẻ loi,
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn thứ N =)))',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn thứ N',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà =)))). Yêu em nhiều nhiều=))))',
-    messLink: 'https://www.facebook.com/leducmanh2907'
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Đừng hết yêu 1 ng cute như mình nha =))).',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //https://www.facebook.com/leducmanh2907
 }
